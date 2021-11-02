641495d67f09791a97918d6cbf7a4e6d89a6cd37
Flávio Silva <Flavio1110@gmail.com>
2021-11-02 13:42:30 +0100

    Use context (#1)
    * use context
    
    * add context
    
    Co-authored-by: Flavio Silva <flavio.silva@leaseplan.com>

---
+++ src/TicTacToeContext.js
@@ -0,0 +1,87 @@
+import { createContext, useContext, useState, useCallback } from "react";
+import {
+  getInitialBoard,
+  cloneBoardMap,
+  isThereAWinner,
+} from "./Services/BoardMapServices";
+
+const initialPlayers = [
+  { symbol: "X", score: 0 },
+  { symbol: "O", score: 0 },
+];
+
+const firstPlayerSymbol = "O";
+const secondPlayerSymbol = "X";
+const initialBoard = getInitialBoard();
+
+const Context = createContext();
+
+const TicTacToeProvider = ({ children }) => {
+  const [currentSymbol, setCurrentSymbol] = useState(firstPlayerSymbol);
+  const [boardMap, setBoardMap] = useState(initialBoard);
+  const [gameIsActive, setGameIsActive] = useState(true);
+  const [players, setPlayers] = useState(initialPlayers);
+
+  const plainAgain = useCallback(() => reset(), []);
+  const resetScoreBoard = useCallback(() => reset(true), []);
+
+  const reset = useCallback((resetScore) => {
+    setCurrentSymbol(firstPlayerSymbol);
+    setBoardMap(initialBoard);
+    setGameIsActive(true);
+
+    if (resetScore) setPlayers(initialPlayers);
+  }, []);
+
+  const setWinner = (symbol) => {
+    const newPlayers = players.map((p) => {
+      return { ...p };
+    });
+
+    const player = newPlayers.find((p) => p.symbol === symbol);
+
+    if (player) {
+      player.score += 1;
+      setPlayers(newPlayers);
+    }
+  };
+
+  const setSelected = (element) => {
+    if (element.selected || !gameIsActive) return;
+
+    const newMap = cloneBoardMap(boardMap);
+    const selectedElement = newMap[element.row][element.column];
+
+    selectedElement.currentSymbol = currentSymbol;
+    selectedElement.selected = true;
+    const someoneWon = isThereAWinner(newMap);
+    if (someoneWon) {
+      setWinner(currentSymbol);
+      setGameIsActive(false);
+      setBoardMap(initialBoard);
+    } else {
+      setCurrentSymbol(
+        currentSymbol === secondPlayerSymbol
+          ? firstPlayerSymbol
+          : secondPlayerSymbol
+      );
+    }
+
+    setBoardMap(newMap);
+  };
+
+  const value = {
+    resetScoreBoard,
+    plainAgain,
+    setSelected,
+    players,
+    boardMap,
+    gameIsActive,
+  };
+
+  return <Context.Provider value={value}>{children}</Context.Provider>;
+};
+
+const useTicTacToeContext = () => useContext(Context);
+
+export { TicTacToeProvider, useTicTacToeContext };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import TicTacToe from "./TicTacToe";
+import { TicTacToeProvider } from "./TicTacToeContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <TicTacToe />
+    <TicTacToeProvider>
+      <TicTacToe />
+    </TicTacToeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
